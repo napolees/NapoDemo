349a1075c8ef5040c35e39856e17e99382e226d9
napolees <769495678@qq.com>
2017-04-12 10:45:14 +0800

    init

---
+++ NapoDemo/NapoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NapoDemo
+//
+//  Created by bjfxtx on 17/4/12.
+//  Copyright © 2017年 FXTX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NapoDemo/NapoDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NapoDemo
+//
+//  Created by bjfxtx on 17/4/12.
+//  Copyright © 2017年 FXTX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
